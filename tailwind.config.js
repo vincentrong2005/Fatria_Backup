ab5c4fd0f7b8035769cb1814f7988a1f32a90f05
vincentrong2005 <41363609+vincentrong2005@users.noreply.github.com>
2025-12-15 20:24:13 +0100

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,vue,js,ts}'],
-
+  content: ['./src/**/*.{html,vue,js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gold: {
+          100: '#fef9c3',
+          200: '#fde68a',
+          400: '#f59e0b',
+          500: '#f59e0b',
+          600: '#d97706',
+          700: '#b45309',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
