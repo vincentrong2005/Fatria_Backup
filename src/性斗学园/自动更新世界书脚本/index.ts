611d02ba757962feaa292f7c5566c565d462014b
Wentao Rong <vincentrong20050210@gmail.com>
2026-01-21 15:44:37 +0100

    版本自动更新

--- src/性斗学园/自动更新世界书脚本/index.ts
+++ src/性斗学园/自动更新世界书脚本/index.ts
@@ -3,7 +3,7 @@ import { compare } from 'compare-versions';
 const CHARACTER_NAME = '性斗学院超级重制版';
 const WORLDBOOK_NAME = '性斗学园';
 const VERSION_ENTRY_NAME = '版本号';
-const GITHUB_BASE_URL = 'https://testingcf.jsdelivr.net/gh/vincentrong2005/Fatria/src/性斗学园';
+const GITHUB_BASE_URL = 'https://raw.githack.com/vincentrong2005/Fatria/main/src/性斗学园';
 
 export async function checkUpdate() {
   const version =
